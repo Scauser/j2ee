0d5c3866147cbdcac6d7eaf8bfa2e3583f782f2e
Daniyar Artykov <redscauser@gmail.com>
2015-12-19 10:28:29 +0600

    sada

--- upwork-jax-ws-client-new/src/main/java/com/upwork/test/MainSolexEdited.java
+++ upwork-jax-ws-client-new/src/main/java/com/upwork/test/MainSolexEdited.java
@@ -38,8 +38,8 @@ public class MainSolexEdited {
 	private static void requestToIWSTrust() {
 		URL url = null;
 		try {
-			//			url = new URL("file:///D:/Projects/Repositories/chrome-extensions/upwork-jax-ws-client-new/src/main/resources/mex/mex.xml");
-			url = new URL("file:///Users/daniyar/repos/j2ee/upwork-jax-ws-client-new/src/main/resources/mex/mex.xml");
+			url = new URL("file:///D:/Projects/Repositories/j2ee/upwork-jax-ws-client-new/src/main/resources/mex/mex.xml");
+			//			url = new URL("file:///Users/daniyar/repos/j2ee/upwork-jax-ws-client-new/src/main/resources/mex/mex.xml");
 			//url = new URL("http://localhost:8080/localhost?WSDL");
 			//			url = new URL("http://localhost:8081/IVR?WSDL");
 			//sample location path can be file:///home/abc/IVRUserManagementServiceImplPort.wsdl
@@ -111,13 +111,16 @@ public class MainSolexEdited {
 		} catch(Exception e) {
 			logger.severe(e.getMessage());
 		}
-		//		RequestSecurityTokenResponseCollectionType rsp = 
-		//				trust13Async.trust13IssueAsync(actionHeader, midHeader, replyTo, vsDebugger, toHeader, security, requestToken);
-		RequestSecurityTokenResponseCollectionType rsp = trust13Async.trust13IssueAsync(requestToken);
+		RequestSecurityTokenResponseCollectionType rsp = 
+				trust13Async.trust13IssueAsync(actionHeader, midHeader, replyTo, vsDebugger, toHeader, security, requestToken);
+		//RequestSecurityTokenResponseCollectionType rsp = trust13Async.trust13IssueAsync(requestToken);
 		List<RequestSecurityTokenResponseType> listRsp = rsp.getRequestSecurityTokenResponse();
+		for(RequestSecurityTokenResponseType rspType : listRsp) {
+			logger.info(rspType.getAny().toString());
+		}
 	}
 
-	private static void callIVR() {
+	private static void requestToIVR() {
 		URL url = null;
 		try {
 			url = new URL("http://localhost:8081/IVR?WSDL");
